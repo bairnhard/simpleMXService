4523c53a71aa1efc81805c17618d2c2bbf4d902a
Bernhard Hecker <bernhard.hecker@gmail.com>
2017-08-22 00:27:37 +0200

    added GeoIP Lookup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # simpleMXService
 super simple REST service to lookup mx records
+
+Usage:
+	http://localhost:8080/getDomain/google.com
+	http://localhost:8080/getProvider/aspmx.l.google.com
+	http://localhost:8080/getGeoIp/aspmx.l.google.com
--- mxservice.go
+++ mxservice.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"net"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -31,23 +33,14 @@ func getProvider(hostname *gin.Context) {
 
 	hoststr := hostname.Params.ByName("hostname")
 
-	// TXT Lookup
-	ipresult, err := net.LookupTXT(hoststr)
-	if err != nil {
-		hostname.JSON(404, "no such host")
-		//panic(err)
-	}
-
-	//IP V4 Lookup
+	// IP V4 Lookup
 	ipv4, err := net.LookupIP(hoststr)
 	if err != nil {
 		hostname.JSON(404, "no such host")
-		//panic(err)
+		panic(err)
 	}
 
 	hostname.JSON(200, ipv4)
-
-	hostname.JSON(200, ipresult)
 
 }
 
@@ -57,7 +50,7 @@ func getMXResults(domain *gin.Context) {
 
 	domainstr := domain.Params.ByName("domain")
 
-	//hier kommt die komplette mx lookup arie
+	//get the MX records
 	MXResult, err := net.LookupMX(domainstr)
 	if err != nil {
 		panic(err)
@@ -71,6 +64,70 @@ func getMXResults(domain *gin.Context) {
 
 //------------------------------------------------------------------------------------------
 
+//GeoIP - here we will get the country of origin...
+// using JSON to GO to convert json to go type struct...
+
+func geoIP(hostname *gin.Context) { //GeoIP
+
+	hoststr := hostname.Params.ByName("hostname")
+
+	type GeoipResult struct {
+		IP          string  `json:"ip"`
+		CountryCode string  `json:"country_code"`
+		CountryName string  `json:"country_name"`
+		RegionCode  string  `json:"region_code"`
+		RegionName  string  `json:"region_name"`
+		City        string  `json:"city"`
+		ZipCode     string  `json:"zip_code"`
+		TimeZone    string  `json:"time_zone"`
+		Latitude    float64 `json:"latitude"`
+		Longitude   float64 `json:"longitude"`
+		MetroCode   int     `json:"metro_code"`
+	}
+
+	url := fmt.Sprintf("http://freegeoip.net/JSON/%s", hoststr)
+
+	// Build the request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+
+	}
+
+	// For control over HTTP client headers,
+	// redirect policy, and other settings,
+	// create a Client
+	// A Client is an HTTP client
+	client := &http.Client{}
+
+	// Send the request via a client
+	// Do sends an HTTP request and
+	// returns an HTTP response
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+
+	}
+
+	// Callers should close resp.Body
+	// when done reading from it
+	// Defer the closing of the body
+	defer resp.Body.Close()
+
+	// Fill the record with the data from the JSON
+	var record GeoipResult
+
+	// Use json.Decode for reading streams of JSON data
+	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+		panic(err)
+	}
+
+	hostname.JSON(200, record)
+
+}
+
+//------------------------------------------------------------------------------------------
+
 func main() {
 
 	router := gin.Default()
@@ -78,9 +135,11 @@ func main() {
 	// Usage:
 	// http://localhost:8080/getDomain/google.com
 	// http://localhost:8080/getProvider/aspmx.l.google.com
+	// http://localhost:8080/getGeoIp/aspmx.l.google.com
 
 	router.GET("/getDomain/:domain", getMXResults)
 	router.GET("/getProvider/:hostname", getProvider)
+	router.GET("/getGeoIp/:hostname", geoIP)
 
 	router.Run()
 
